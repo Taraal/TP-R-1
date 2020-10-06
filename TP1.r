27408e25e2a5f45724055893a0b64d88b23866fd
T4raal@gmail.com <Vw9W42YXRnMe>
2020-10-06 12:49:56 +0200

    Added Clément's work

--- TP1.r
+++ TP1.r
@@ -7,17 +7,8 @@ words = scan(file="test.txt", what="character")
 
 #Putting all the words in a table, then sorting it by number of occurrences. 
 # Get the top 10 words ([1:10])
-sorted_words = sort(table(words), decreasing = T)[1:10]
+words
+sorted_words = sort(table(words), decreasing = T)
+sorted_words
 
-
-sorted_words_frame = data.frame(sort(table(words), decreasing = T)[1:30])
-ggplot(data=sorted_words_frame) + geom_col(aes(x=words, y=Freq))
-
-
-
-full_corpus = Corpus(DataframeSource(sorted_words))
-termMatrix = DocumentTermMatrix(full_corpus, control=list(wordLengths=c(1,Inf)))
-
-X = inspect(termMatrix)
-out = crossprod(X)
-out
+table(sorted_words)
